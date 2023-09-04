72deb9047c772b75dbc0f0c09c27beb042a7394a
Alan Shieh <piratejing41@gmail.com>
2023-09-03 21:16:30 -0700

    added meme data and styling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,27 @@
 import "../blocks/Meme.css";
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+
+    setMemeImage(url);
+  }
   return (
-    <div className="meme-container">
-      <form className="form">
+    <main className="meme-container">
+      <div className="form">
         <input className="form-input" type="text" placeholder="Top Text" />
         <input className="form-input" type="text" placeholder="Bottom Text" />
-        <button className="form-button">Get a new meme image</button>
-      </form>
-    </div>
+        <button className="form-button" onClick={getMemeImage}>
+          Get a new meme image
+        </button>
+      </div>
+      <img className="meme-image" src={memeImage} />
+    </main>
   );
 }
