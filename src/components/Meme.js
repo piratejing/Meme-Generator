0a5862f4487fb58743dec4ae8c789a6a729423c6
Alan Shieh <piratejing41@gmail.com>
2023-09-19 21:44:16 -0700

    added text input states

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -19,16 +19,28 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main className="meme-container">
       <div className="form">
-        <input className="form-input" type="text" placeholder="Top Text" />
-        <input className="form-input" type="text" placeholder="Bottom Text" />
+        <input className="form-input" type="text" placeholder="Top Text" name="topText" value={meme.topText} onChange={handleChange} />
+        <input className="form-input" type="text" placeholder="Bottom Text" name="bottomText" value={meme.bottomText} onChange={handleChange} />
         <button className="form-button" onClick={getMemeImage}>
           Get a new meme image
         </button>
       </div>
-      <img className="meme-image" src={meme.randomImage} />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme-image" />
+        <h2 className="meme-text top">{meme.topText}</h2>
+        <h2 className="meme-text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
